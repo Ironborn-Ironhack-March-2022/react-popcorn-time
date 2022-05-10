576527d07dd228c7e33a4ebaea3a5b33c6795df1
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-05-10 17:44:58 +0200

    add button to display only top rated movies

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import moviesArr from "../data/movies.json";
 import Movie from "./Movie";
 
 function Main() {
 
+    const [movies, setMovies] = useState(moviesArr);
+
+
+    const filterTopRatedOnly = () => {
+        
+        // option 1: we pass the new value
+        // setMovies(newListOfMovies);
+        
+        // option 2: we pass a function (functional update)
+        setMovies( (prevValue) => {
+            const newListOfMovies = prevValue.filter( (movie) => {
+                return movie.rating > 8;
+            });
+            return newListOfMovies;
+        });
+
+    }
 
     return (
         <>
-            {moviesArr.map((element) => {
+
+            <div className="controls">
+                <button onClick={filterTopRatedOnly}>Display Top Rated Only</button>
+            </div>
+
+            {movies.map((element) => {
                 return (
                     <Movie 
                         key={element.id} 
