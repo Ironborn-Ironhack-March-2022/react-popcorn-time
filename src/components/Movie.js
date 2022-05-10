8eddff59f676bb0d9f320bd05028065d69c2f607
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-05-10 16:58:12 +0200

    conditional rendering: opt2

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,13 @@
 function Movie(props) {
 
-    let content;
-
-    if(props.rating > 8) {
-        content = <span>RECOMMENDED</span>
-    } else {
-        content = "";
-    }
-
     return (
         <div>
             <h3>{props.title}</h3>
             <p>
                 Year: {props.year} <br />
                 Rating: {props.rating} <br />
-                {content}
+                
+                { props.rating > 8 && <span>RECOMMENDED</span>}
             </p>
         </div>
     );
